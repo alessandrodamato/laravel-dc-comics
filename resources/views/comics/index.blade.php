08b27f844a69895074ca908269c99b2d0915ca1a
alessandrodamato <alessandro042004@gmail.com>
2024-05-16 17:21:19 +0200

    [add] edit/delete buttons

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -20,7 +20,7 @@
 
     @forelse ($data as $comic)
     <div class="col-3">
-      <div class="card mb-5 p-3" style="width: 18rem; height: 800px">
+      <div class="card mb-5 p-3" style="width: 18rem; height: 900px">
         <img src="{{$comic->thumb}}" class="card-img-top w-100 h-100 object-fit-cover" alt="{{$comic->title}}">
         <div class="card-body">
           <h5 class="card-title">{{$comic->title}}</h5>
@@ -30,10 +30,16 @@
           <li class="list-group-item"><strong>Serie: </strong>{{$comic->series}}</li>
           <li class="list-group-item"><strong>Data di uscita: </strong>{{$comic->sale_date}}</li>
           <li class="list-group-item text-capitalize"><strong>Categoria: </strong>{{$comic->type}}</li>
+          <li class="list-group-item text-capitalize">
+            <a href="{{route('comics.show', $comic)}}" class="card-link btn btn-success">Vedi dettagli</a>
+          </li>
+          <li class="list-group-item text-capitalize">
+            <a href="#" class="card-link btn btn-warning">Modifica</a>
+          </li>
+          <li class="list-group-item text-capitalize">
+            <a href="#" class="card-link btn btn-danger">Elimina</a>
+          </li>
         </ul>
-        <div class="card-body">
-          <a href="{{route('comics.show', $comic)}}" class="card-link btn btn-success">Vedi dettagli</a>
-        </div>
       </div>
     </div>
     @empty
